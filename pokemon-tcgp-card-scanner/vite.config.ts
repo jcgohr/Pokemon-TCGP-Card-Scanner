a1b0c1af993294f5e158cd6da8aec22f15d4d299
Lucian <medrihanlucian@gmail.com>
2025-03-10 21:04:59 +0100

    deploy

--- pokemon-tcgp-card-scanner/vite.config.ts
+++ pokemon-tcgp-card-scanner/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Pokemon-TCGP-Card-Scanner/"
+  base:"/Pokemon-TCGP-Card-Scanner/pokemon-tcgp-card-scanner/"
 })
